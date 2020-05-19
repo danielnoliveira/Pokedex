db7623d08b3c53e241d1aaeb586adc0115f19600
Daniel Nogueira de Oliveira <dan.knightcode@gmail.com>
2020-05-19 02:10:21 -0300

    Logo pokemon adicionado.Campo para entrada de texto adicionado.Tudo estilizado.Proximo passo:Lista de pokemons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App__header">
+          <img src="https://pluspng.com/img-png/pokemon-logo-png-pokemon-logo-png-2000.png" alt="pokemon logo"/>
+          <input className="header__inputSearch" type="text" name="pokemon" placeholder="Pokemon name"/>
+        </header>
     </div>
   );
 }
