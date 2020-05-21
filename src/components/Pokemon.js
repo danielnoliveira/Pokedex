61cad661b2cd284eada70b808ec9434f6185caf2
Daniel Nogueira de Oliveira <33916785+danielnoliveira@users.noreply.github.com>
2020-05-21 19:43:25 -0300

    Update Pokemon.js
    Tentando consertar o bug, tentativa final.

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './Pokemon.css';
 import PokemonInfo from './PokemonInfo.js';
 
-export default function Pokemon({pokemon}) {
+export default function Pokemon({pokemon,newid}) {
     return (
         <div className={`pokemon ${pokemon.types[pokemon.types.length-1]["type"]["name"]}`}>
             <img src={require(`../images/${pokemon.avatar}`)} alt="pokemon front"/>
             <span className="pokemon__name">{pokemon.name}</span>
-            <PokemonInfo id={pokemon.id} types={pokemon.types} height={pokemon.height} weight={pokemon.weight} stats={pokemon.stats}/>
+            <PokemonInfo newid={newid} types={pokemon.types} height={pokemon.height} weight={pokemon.weight} stats={pokemon.stats}/>
         </div>
     );
 }
