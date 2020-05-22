3f9f5dd3f0b0484257b7cc74101a0877dc4fd4d0
Daniel Nogueira de Oliveira <dan.knightcode@gmail.com>
2020-05-21 21:28:45 -0300

    Altura e peso dos pokemons formatados para metro e quilograma, respectivamente.

--- src/components/SecondaryStats.js
+++ src/components/SecondaryStats.js
@@ -3,8 +3,8 @@ import './SecondaryStats.css';
 export default function SecondaryStats({height,weight}) {
     return(
         <div className="pokemon__secondaryStats">
-            <span className="secondaryStats__height">Height:{height}</span>
-            <span className="secondaryStats__weight">Weight:{weight}</span>
+            <span className="secondaryStats__height">Height: {height/10} <span style={{textTransform:"lowercase"}}>m</span></span>
+            <span className="secondaryStats__weight">Weight: {weight/10} Kg</span>
         </div>
     );
 }
